3f988204b16cedc84029ae09e457ff24744a7126
E Rodrigo Lopes <e.rodrigo.l@outlook.com>
2025-05-23 15:29:21 -0300

    chore(config): ➕ adicionar alias `@utils` no Babel e TypeScript
    - Atualiza `babel.config.js` e `tsconfig.json` para incluir o alias `@utils`.
    - Permite importar arquivos utilitários com caminho simplificado: `@utils/*`.
    
    Essa alteração melhora a legibilidade e organização do código.

--- babel.config.js
+++ babel.config.js
@@ -22,6 +22,7 @@ module.exports = function (api) {
             '@store': './store',
             '@theme': './theme',
             '@tests': './tests',
+            '@utils': './utils',
           },
         },
       ],
