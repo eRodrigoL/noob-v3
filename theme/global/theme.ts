f071ff21662051d3ffa098ddfea70fd80e2d420d
KETHILYN <Kethilyn_francielle@outlook.com>
2025-06-09 19:09:21 -0300

    Resolvendo cores do tema dark

--- theme/global/theme.ts
+++ theme/global/theme.ts
@@ -5,17 +5,17 @@
 // Define o tema "ligth" (tema claro)
 
 const theme = {
-
+//
 light: {
     name: 'Light',
-    backgroundBase: '#F8F8F8', // Cor de fundo principal: Cinza claro sofisticado (para uma base neutra)
-    backgroundSemiHighlight: '#DADADA', // Cor para destaque secundário: Cinza médio (para contraste suave)
-    backgroundHighlight: '#F35B04', // Cor para destaque principal: Laranja quente e vibrante
+    backgroundBase: '#FAF5F0', // Cor de fundo principal
+    backgroundSemiHighlight: '#F8E7D4', // Cor para destaque secundário
+    backgroundHighlight: '#F35B04', // Cor para destaque principal
     textHighlight: '#00004C', // Cor para texto destacado
-    textOnBase: '#000000', // Cor para texto sobre o fundo principal
-    textOnSemiHighlight: '#3D348B', // Cor para texto sobre o destaque secundário: Azul profundo, criando contraste...
-    textOnHighlight: '#FFFFFF', // Cor para texto sobre o destaque principal: Branco para ter um contraste nítido
-    border: '#5759B2', // Cor para bordas
+    textOnBase: '#000048', // Cor para texto sobre o fundo principal
+    textOnSemiHighlight: '#3D348B', // Cor para texto sobre o destaque secundário
+    textOnHighlight: '#FFFFFF', // Cor para texto sobre o destaque principal
+    border: '#F35B04', // Cor para bordas
     shadow: 'rgba(0, 0, 0, 0.15)', // Sombra: Sutil para adicionar profundidade
     overlay: 'rgba(0, 0, 0, 0.3)', // Sobreposição: Transparência discreta pensando em ter foco
     accentColor: '#F7B801', // Cor de destaque adicional: Amarelo para elementos chamativos
@@ -27,11 +27,11 @@ light: {
   dark: {
     name: 'Dark',
     backgroundBase: '#1E1E24',
-    backgroundSemiHighlight: '#FFB27A',
-    backgroundHighlight: '#F35B04',
+    backgroundSemiHighlight: '#2E2B27',
+    backgroundHighlight: '#602401',
     textHighlight: '#FFFFFF',
-    textOnBase: '#FFEE93',
-    textOnSemiHighlight: '#00004C',
+    textOnBase: '#BFBCB7',
+    textOnSemiHighlight: '#BFBCB7',
     textOnHighlight: '#FFFFFF',
     border: '#F18701',
     shadow: 'rgba(0, 0, 0, 0.8)',
